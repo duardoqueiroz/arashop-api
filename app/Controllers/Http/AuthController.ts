d9d84b645fdb72e8266a7a2f7f8251f3755fc95f
duardoqueiroz <eduardoldq1@gmail.com>
2022-12-05 20:29:08 -0300

    feat: authentication

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,20 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
+export default class AuthController {
+  public async store({ request, response, auth }: HttpContextContract) {
+    try {
+      const { email, password } = request.all()
+      const token = await auth.attempt(email, password, { expiresIn: '7days' })
+      let user: User = token.user
+      return response.ok({ user, token })
+    } catch (error) {
+      return response.unauthorized({ message: 'Falha na autenticação', error })
+    }
+  }
+
+  public async destroy({ auth, response }: HttpContextContract) {
+    await auth.logout()
+    return response.ok('ok')
+  }
+}
