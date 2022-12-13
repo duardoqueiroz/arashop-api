7693e3d52bc0c6370d465132c7fe7d106b9dbf91
duardoqueiroz <eduardoldq1@gmail.com>
2022-12-13 14:23:06 -0300

    feat: user items relationship

--- app/Controllers/Http/UserItemsController.ts
+++ app/Controllers/Http/UserItemsController.ts
@@ -1,15 +1,27 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Item from 'App/Models/Item'
+import User from 'App/Models/User'
+import StoreValidator from 'App/Validators/UserItem/StoreValidator'
 
 export default class UserItemsController {
-  public async index({}: HttpContextContract) {}
+  public async index({response, params, auth}: HttpContextContract) {
+    const items = await auth.user!.load("items")
+    return response.ok(items)
+  }
 
-  public async create({}: HttpContextContract) {}
+  public async store({response,request,auth}: HttpContextContract) {
+    const data = await request.validate(StoreValidator)
+    await auth.user!.related("items").sync(data.items.map((i)=> i.item_id))
+  }
 
-  public async store({}: HttpContextContract) {}
-
-  public async show({}: HttpContextContract) {}
-
-  public async edit({}: HttpContextContract) {}
+  public async show({response,params, auth}: HttpContextContract) {
+    await auth.user!.load("items")
+    const item = auth.user!.items.find((i)=> i.id === params.item_id)
+    if(!item){
+      return response.notFound("Item não encontrado!")
+    }
+    return response.ok(item)
+  }
 
   public async update({}: HttpContextContract) {}
 
--- app/Validators/UserItem/StoreValidator.ts
+++ app/Validators/UserItem/StoreValidator.ts
@@ -1,18 +1,21 @@
 import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { ITEM_STATUS } from 'Contracts/enums/ITEM_STATUS'
 
 export default class StoreValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    item_id: schema.number([rules.exists({ table: 'items', column: 'id' }), rules.required()]),
-    quantity: schema.number([rules.range(1, 100), rules.required()]),
+    items: schema.array([rules.required(), rules.minLength(1)]).members(
+      schema.object().members(
+        {
+          item_id: schema.number([rules.exists({ table: 'items', column: 'id' }), rules.required()]),
+          quantity: schema.number([rules.range(1, 100)]),
+          status: schema.enum([ITEM_STATUS.BOUGHT,ITEM_STATUS.SAVED])
+        }
+      )
+    )
   })
 
-  public messages: CustomMessages = {
-    'item_id.exists': 'Item não encontrado',
-    'item.required': 'Item é obrigatório',
-    'quantity.range': 'Quantidade deve ser entre 1 e 100',
-    'quantity.required': 'Quantidade é obrigatório',
-  }
+  public messages: CustomMessages = { }
 }
